c3ae537516c4d4be2d4a99349921b778159de851
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2023-02-13 16:21:54 +0530

    CustomCursor

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // ==========================================
+//  Custom Cursor
+// ==========================================
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
+// ==========================================
 //  Creating a Responsive navbar
 // ==========================================
 
